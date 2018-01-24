757c0e901eb16ea349232568ecb24661779f357f
craig stevens <c_stevens44@hotmail.com>
2018-01-24 04:13:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, updat) {
 }
 
 function removeFromPlaylist(playlist,artistName) {
-Object.assign({"kanye": "Gold digger"}, {})
+delete playlist.Kanye.Gold Digger;
 return playlist
   
 }
