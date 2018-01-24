ce2277c85ce2b59a0e1f0078976fbcc7c7a0d447
craig stevens <c_stevens44@hotmail.com>
2018-01-24 03:49:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = { "Linken Park": "Numb"};
 function updatePlaylist(playlist, updat) {
   return Object.assign({}, playlist, {"Phil Ochs": "Here's to the State of Mississippi"});
 }
+
+function removeFromPlaylist(playlist,artistName) {
+  return delete playlist.
+}
